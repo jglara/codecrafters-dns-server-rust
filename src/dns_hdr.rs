1a09c3e0b740e91d96e230a8611765a8153ddb77
jglara <jglara@gmail.com>
2024-02-14 11:58:33 +0100

    Write header section

---
+++ src/dns_hdr.rs
@@ -0,0 +1,83 @@
+use anyhow::Result;
+
+/*
+                                    1  1  1  1  1  1
+      0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                      ID                       |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |QR|   Opcode  |AA|TC|RD|RA|   Z    |   RCODE   |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                    QDCOUNT                    |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                    ANCOUNT                    |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                    NSCOUNT                    |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                    ARCOUNT                    |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+     */
+
+use bytes::{Buf, BufMut, Bytes, BytesMut};
+
+const DNS_HDR_SIZE: usize = 12;
+
+#[derive(Debug)]
+pub struct DNSHdr {
+    pub id: u16,
+    flags: u16,
+    pub qdcount: u16,
+    pub ancount: u16,
+    pub nscount: u16,
+    pub arcount: u16,
+}
+
+impl DNSHdr {
+    pub fn new_response (id: u16) -> Self {
+        DNSHdr {
+            id: id,
+            flags: DNSHdr::flags(1,0,0,0,0,0,0),
+            qdcount: 0,
+            ancount: 0,
+            nscount: 0,
+            arcount: 0,
+        }
+    }
+
+    pub fn flags(qr: u8, opcode: u8, aa: u8, tc: u8, rd: u8, ra: u8, rcode: u8) -> u16 {
+
+        let flags_h: u8 = (qr << 7) | (opcode << 3) | (aa<<2) | (tc<<1) | rd;
+        let flags_l: u8 = (ra << 7) | (rcode);
+
+        (flags_h as u16) << 8 | (flags_l as u16)
+
+
+    }
+
+    pub fn to_bytes(&self) -> Bytes {
+        let mut buf: BytesMut = BytesMut::with_capacity(DNS_HDR_SIZE);
+
+        buf.put_u16_ne(self.id);
+        buf.put_u16_ne(self.flags);
+        buf.put_u16_ne(self.qdcount);
+        buf.put_u16_ne(self.ancount);
+        buf.put_u16_ne(self.nscount);
+        buf.put_u16_ne(self.arcount);
+
+        buf.freeze()
+
+    }
+
+    pub fn from_bytes(mut buf: &[u8]) -> Result<Self> {
+        anyhow::ensure!(buf.len()>=DNS_HDR_SIZE);
+
+        Ok(Self {
+            id     : buf.get_u16_ne(),
+            flags  : buf.get_u16_ne(),
+            qdcount: buf.get_u16_ne(),
+            ancount: buf.get_u16_ne(),
+            nscount: buf.get_u16_ne(),
+            arcount: buf.get_u16_ne(),
+        })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
-
+use anyhow::Result;
 use std::net::UdpSocket;
+mod dns_hdr;
 
-fn main() {
+use dns_hdr::DNSHdr;
+
+fn main() -> Result<()> {
     let udp_socket = UdpSocket::bind("127.0.0.1:2053").expect("Failed to bind to address");
-    let mut buf = [0; 512];
-        loop {
+    let mut buf = [0;512];
+
+    loop {
         match udp_socket.recv_from(&mut buf) {
             Ok((size, source)) => {
                 println!("Received {} bytes from {}", size, source);
-                let response = [];
-                udp_socket
-                    .send_to(&response, source)
-                    .expect("Failed to send response");
+                if let Ok(request) = DNSHdr::from_bytes(&buf) {
+                    eprintln!("Received DNS query: {request:?}");
+                    let response = DNSHdr::new_response(request.id);
+
+                    udp_socket
+                        .send_to(&response.to_bytes(), source)
+                        .expect("Failed to send response");
+                };
             }
             Err(e) => {
                 eprintln!("Error receiving data: {}", e);
@@ -19,4 +27,6 @@ fn main() {
             }
         }
     }
+
+    Ok(())
 }
